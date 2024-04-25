375bc8327b7514d906b2dd745c4a98b9f8ec7023
lucio-crDEV <lucio.m.cardenas@gmail.com>
2024-04-25 16:35:43 -0400

    fix en el UsuarioSchema, habian errores tipográficos. Remplazando valor _id por uid

--- models/usuario.js
+++ models/usuario.js
@@ -4,29 +4,38 @@ const { Schema, model } = require('mongoose');
 const UsuarioSchema = Schema({
     nombre: {
         type: String,
-        require: true
+        required: true
     },
     email: {
         type: String,
-        require: true,
+        required: true,
         unique: true
     },
     password: {
         type: String,
-        require: true,
+        required: true,
     },
     img: {
         type: String
     },
     role: {
         type: String,
-        require: true,
-        defaul: 'USER_ROLE'
+        required: true,
+        default: "USER_ROLE"
     },
     google: {
         type: Boolean,
         default: false
     },
 })
+
+
+// Arreglo por si quisiera cambiaar _id por uid en el response de getUsuarios   
+UsuarioSchema.method('toJSON', function() {
+    const { __v, _id, ...object } = this.toObject();
+
+    object.uid = _id
+    return object
+});
 
 module.exports = model( 'Usuario', UsuarioSchema );
