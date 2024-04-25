cae10c14b4751cd1ad6d514c4818463862df642e
lucio-crDEV <lucio.m.cardenas@gmail.com>
2024-04-25 16:36:16 -0400

    aplicando ruta post con sus respectivos controllers

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,11 +3,13 @@
 */
 
 const { Router } = require('express');
-const { getUsuarios } = require('../controllers/usuarios')
+const { getUsuarios, crearUsuario } = require('../controllers/usuarios')
 
 const router = Router();
 
 router.get('/', getUsuarios);
+
+router.post('/', crearUsuario)
 
 
 module.exports = router;
