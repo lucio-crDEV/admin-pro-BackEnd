7e9bb6ee882184bd9d77288c4f2626c2d5b7d1ed
lucio-crDEV <lucio.m.cardenas@gmail.com>
2024-04-25 14:18:53 -0400

    configurcion inicial

---
+++ database/config.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const dbConnection = async () => {
+
+    try {
+        await mongoose.connect( process.env.DB_CNN );
+
+        console.log('✅ DB Online')
+
+    } catch (error) {
+        console.log(error)
+        throw new Error('Error a la hora de iniciar la DB ver logs.')
+    }
+};
+
+module.exports = {
+    dbConnection
+};
+
