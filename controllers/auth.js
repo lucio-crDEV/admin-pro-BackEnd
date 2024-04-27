fa4f6b1788894fe6515dbf4e444e28214fc056c2
lucio-crDEV <lucio.m.cardenas@gmail.com>
2024-04-27 11:45:20 -0400

    logica e implementación de la estructura lógica básica par a el login, se verifica correo,contraseña y se controlan errores con bloque try/catch

---
+++ controllers/auth.js
@@ -0,0 +1,51 @@
+const { response } = require('express');
+const bcrypt = require('bcryptjs');
+
+const Usuario = require('../models/usuario');
+
+const loginUser = async ( req, res = response ) => { 
+    
+    const { email, password } = await req.body;
+    try {
+        // Se puede agregar unos segundos de retraso para no dar pistas de que es lo que esta realmente inválido en el proceso de log
+        
+        // Verificar email
+        const usuarioDB = await Usuario.findOne({ email });
+        
+        if( !usuarioDB ){
+            return res.status(404).json({
+                ok: false,
+                msg: 'Usuario y/o contraseña no válido'
+            })
+        }
+
+        // Verificar contraseña
+        const validPassword = bcrypt.compareSync( password, usuarioDB.password );
+        if( !validPassword ){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Usuario y/o contraseña no válido'
+            })
+        }
+
+        // Generar JWT
+        
+
+        res.status(200).json({
+            ok: true,
+            msg: 'Hola mundo'
+        });
+    } catch ( error ) {
+        console.log( error )
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el admin'
+        })
+    };
+
+    
+}
+
+module.exports = {
+    loginUser
+}
