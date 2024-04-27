744806d387b2df98432876abe0e99c45ede6d958
lucio-crDEV <lucio.m.cardenas@gmail.com>
2024-04-27 11:40:14 -0400

    implementando bloques try/catch en funciones async para los controllers del CRUD en la base de datos

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,8 +1,11 @@
+const { response } = require('express');
+const bcrypt = require('bcryptjs');
+
 const Usuario = require('../models/usuario');
 
-const getUsuarios = async (req, res) => {
+const getUsuarios = async (req, res = response) => {
 
-    const usuarios = await Usuario.find({}, 'nombre email role google' );
+    const usuarios = await Usuario.find({}, 'nombre email role google');
 
     res.json({
         usuarios
@@ -10,25 +13,125 @@ const getUsuarios = async (req, res) => {
 
 };
 
-const crearUsuario = async (req, res) => {
-    
+const crearUsuario = async (req, res = response) => {
+
     const { email, password, nombre } = req.body;
+
+    try {
+        const existeEmail = await Usuario.findOne({ email });
+
+        if (existeEmail) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya está registrado.'
+            });
+        }
+
+        const usuario = new Usuario(req.body);
+
+        // Encriptar contraseña en un hash de una sola vía
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
+
+        // Guardar usuario con contraseña encriptada
+        await usuario.save();
+
+        res.json({
+            ok: true,
+            usuario
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado, revisar logs'
+        })
+    };
+
+};
+
+const actualizarUsuario = async (req, res = response) => {
+    // TODO validar token y comprobar si es el usuario correcto
+
+    const uid = req.params.id;
     
-    // sin validacion se va a pulir más adelante
+    try {
 
-    const usuario = new Usuario( req.body );
+        const usuarioDB = await Usuario.findById(uid);
 
-    await usuario.save();
+        if ( !usuarioDB ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe un usuario con ese id'
+            });
+        }
 
+        // Actualizaciones
+        const { password, google, email, role, ...campos } = req.body;
 
-    res.json({
-        ok: true,
-        usuario
-    })
+        if (usuarioDB.email !== email) {
+
+            const existeEmail = await Usuario.findOne({ email });
+            if ( existeEmail ) {
+                return res.status(400).json({
+                    ok: false,
+                    msg: 'Ya existe un usuario con ese email'
+                });
+            }
+        }
+
+        campos.email = email;
+        campos.role = usuarioDB.role;
+        const usuarioActualizado = await Usuario.findByIdAndUpdate(uid, campos, { new: true });
+
+        res.json({
+            ok: true,
+            usuario: usuarioActualizado
+        })
+
+    } catch ( error ) {
+        console.log( error )
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado'
+        })
+    }
+};
+
+const borrarUsuario = async (req, res = response) => {
+
+    const uid = req.params.id;
+    
+    try {
+
+        const usuarioDB = await Usuario.findById(uid);
+        if( !usuarioDB ){
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe un usuario para esa id'
+            });
+        }
+
+        await Usuario.findByIdAndDelete( uid );
+
+        res.status(400).json({
+            ok: true,
+            msg: 'Usuario eliminado'
+        });
+    } catch ( error ) {
+        console.log( error )
+        res.status(400).json({
+            ok: false,
+            msg: 'Hable con el administrador.'
+        });
+    }
 };
 
 
 module.exports = {
     getUsuarios,
-    crearUsuario
+    crearUsuario,
+    actualizarUsuario,
+    borrarUsuario,
 }
