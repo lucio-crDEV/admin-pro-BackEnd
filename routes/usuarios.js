55d7c87fb3416e181e29ad905a941868c032eb28
lucio-crDEV <lucio.m.cardenas@gmail.com>
2024-04-27 11:38:58 -0400

    definiendo middlewares de librerias y personalizados, y controladores para las rutas post, put, delete

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,15 +1,43 @@
 /* 
     Ruta: /api/usuarios
 */
-
 const { Router } = require('express');
-const { getUsuarios, crearUsuario } = require('../controllers/usuarios')
+const { check } = require('express-validator');
+
+const { validarCampos } = require('../middlewares/validar-campos');
+const { getUsuarios, crearUsuario, actualizarUsuario, borrarUsuario } = require('../controllers/usuarios');
 
 const router = Router();
 
 router.get('/', getUsuarios);
 
-router.post('/', crearUsuario)
+/* 
+    Middleware para verificar que venga la info.
+        router.post(Ruta, [Middleware1, Middleware2, Middleware3, MiddlewarePersonalizado] , Controlador); 
+*/
+
+router.post('/',
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('password', 'La contraseña es obligatoria').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        validarCampos,
+    ],
+    crearUsuario
+);
+
+router.put('/:id',
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('role', 'El rol es obligatorio').isEmail(), 
+        // validarCampos
+    ],
+    actualizarUsuario
+);
+router.delete('/:id', 
+    borrarUsuario 
+);
 
 
 module.exports = router;
